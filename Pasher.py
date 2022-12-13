6c012b64abbb89fa1bd1372ee84bee122962403a
Ava8790 <ava87900@gmail.com>
2022-12-13 21:30:21 +0100

    Removed Caching

--- Pasher.py
+++ Pasher.py
@@ -1,8 +1,8 @@
 import sys, hashlib, colorama
 from threading import Thread
-print("Pasher from 3rd December 2022")
+print("Pasher from 13rd December 2022")
 if sys.argv[-1] == "-h" or sys.argv[-1] == "--help" or len(sys.argv) < 2: print("Correct usage: script hashesfile wordlist");exit()
-hashfile = open(sys.argv[1], 'r', encoding='utf8');wordlist = open(sys.argv[2], 'r', encoding='latin1');words = [];hashes = [];donehashes = [];count = 0;hashes1 = 0
+hashfile = open(sys.argv[1], 'r', encoding='utf8');words = open(sys.argv[2], 'r', encoding='latin1');count = 0;hashes1 = 0
 def calculateAlgorithm(hash):
     if len(hash) == 32: return "MD5"
     elif len(hash) == 40: return "SHA1"
@@ -20,31 +20,14 @@ def checkHash(hash, word):
     elif calculateAlgorithm(hash) == "SHA512": return hashlib.sha512(word.strip().encode()).hexdigest() == hash
     else: return False
 print(f"{colorama.Fore.WHITE}LOG: {colorama.Fore.RESET}Reading...")
-for line in wordlist:
-    if line == "": continue
-    if line.startswith(";"): continue
-    count = count + 1;words.append(line)
-for line in hashfile:
-    if line == "": continue
-    if line.startswith(";"): continue
-    hashes1 = hashes1 + 1;hashes.append(line)
-print(f"{colorama.Fore.WHITE}LOG: {colorama.Fore.RESET}Found {hashes1} hashes and {count} words.")
-hashesleft = hashes1 - 1
-for hash in hashes: hashes[hashes.index(hash)] = hash.strip()
 def crack(word, hash):
     if checkHash(hash.strip(), word.strip()):
-        global hashesleft
-        global donehashes
         print(f"{colorama.Fore.BLUE}INFO: {colorama.Fore.RESET}Found {hash.strip()}: {word.strip()}")
-        donehashes.append(hash)
-        hashesleft = hashesleft - 1
 for word in words:
-    for hash in hashes:
+    if word == "": continue
+    if word.startswith(";"): continue
+    for hash in hashfile:
+        if hash == "": continue
+        if hash.startswith(";"): continue
         Thread(target=crack, args=(word.strip(), hash)).start()
-        if hashesleft == 0: wordlist.close();print(f"{colorama.Fore.GREEN}SUCCESS: {colorama.Fore.RESET}Done.");exit()
-wordlist.close();hashesstr = ""
-for hash in donehashes: hashes.remove(hash)
-for hash in hashes:
-    if hashes[hashes.index(hash)] == hashes[-1]: hashesstr = hashesstr + f"{hash.strip()}"
-    else: hashesstr = hashesstr + f"{hash.strip()}, "
-if hashesleft != 0: print(f"{colorama.Fore.YELLOW}WARN: {colorama.Fore.RESET}Failed to get: {hashesstr}")
+words.close();hashesstr = ""
