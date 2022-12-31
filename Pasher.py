c814226125af2e6d51e1176e6f448033c22c88c7
Ava8790 <ava87900@gmail.com>
2022-12-31 13:34:02 +0100

    some improvents and added sha384 in the example

--- Pasher.py
+++ Pasher.py
@@ -1,8 +1,7 @@
 import sys, hashlib, colorama
 from threading import Thread
-print("Pasher from 13rd December 2022")
+print("Pasher from 31rd December 2022")
 if sys.argv[-1] == "-h" or sys.argv[-1] == "--help" or len(sys.argv) < 2: print("Correct usage: script hashesfile wordlist");exit()
-hashfile = open(sys.argv[1], 'r', encoding='utf8');words = open(sys.argv[2], 'r', encoding='latin1');count = 0;hashes1 = 0
 def calculateAlgorithm(hash):
     if len(hash) == 32: return "MD5"
     elif len(hash) == 40: return "SHA1"
@@ -19,10 +18,10 @@ def checkHash(hash, word):
     elif calculateAlgorithm(hash) == "SHA384": return hashlib.sha384(word.strip().encode()).hexdigest() == hash
     elif calculateAlgorithm(hash) == "SHA512": return hashlib.sha512(word.strip().encode()).hexdigest() == hash
     else: return False
-print(f"{colorama.Fore.WHITE}LOG: {colorama.Fore.RESET}Reading...")
 def crack(word, hash):
-    if checkHash(hash.strip(), word.strip()):
-        print(f"{colorama.Fore.BLUE}INFO: {colorama.Fore.RESET}Found {hash.strip()}: {word.strip()}")
+    if checkHash(hash.strip(), word.strip()): print(f"{colorama.Fore.BLUE}INFO: {colorama.Fore.RESET}Found {hash.strip()} ({calculateAlgorithm(hash.strip())}): {word.strip()}")
+print("Checking...")
+hashfile = open(sys.argv[1], 'r', encoding='utf8');words = open(sys.argv[2], 'r', encoding='latin1')
 for word in words:
     if word == "": continue
     if word.startswith(";"): continue
